703d93f2227b37ae59d93551f20d498a9eb2d1db
Alessandro <alessandro.rotili@gmail.com>
2017-12-02 11:43:57 +0100

    Settnig up uniform rectilinear motion

---
+++ Assets/Scripts/UniformRectilinear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniformRectilinear : MonoBehaviour {
+
+    Rigidbody2D rb;
+    float time = 0.000f;
+    float velocity;
+    float displacement;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+        {
+            rb.position = new Vector2(13f, transform.position.y);
+        }
+	}
+}
